757e3f67760bed3a92cebf32b3842fb9aa91c735
jeffrey.geyssens <jeffrey@humanized.be>
2016-02-03 19:30:01 +0100

    [ADD] Form Selection Widget

---
+++ components/LocationSelector.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace humanized\location\components;
+
+use humanized\location\models\location\Country;
+use kartik\depdrop\DepDrop;
+use kartik\select2\Select2;
+use yii\base\Widget;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Url;
+
+/**
+ * LanguagePicker Widget for Yii2 - By Humanized
+ * 
+ * This widget allows the user to set the application using the configuration provided by the Module. 
+ * 
+ * @name Yii2 LanguagePicker Widget Class
+ * @version 0.1 
+ * @author Jeffrey Geyssens <jeffrey@humanized.be>
+ * @package yii2-translation
+ */
+class LocationSelector extends Widget {
+
+    public $form = NULL;
+    public $model = NULL;
+    public $enableBootstrap = FALSE;
+    public $countryTemplate = '{}{}{}';
+    public $cityTemplate = '{}{}';
+
+    public function init()
+    {
+        parent::init();
+    }
+
+    public function run()
+    {
+        $out = '';
+        $countryDropdown = Select2::widget([
+                    'name' => 'country',
+                    'value' => '',
+                    'data' => ArrayHelper::map(Country::available(), 'iso_2', 'iso_3'),
+                    'options' => [ 'label' => 'yaya', 'placeholder' => 'Select Country ...', 'id' => 'country-selection']
+        ]);
+
+        $locationDropdown = $this->form->field($this->model, 'location_id')->label(false)->widget(DepDrop::classname(), [
+            'type' => DepDrop::TYPE_SELECT2, //For Select2
+            'options' => ['id' => 'location-selection'],
+            'select2Options' => [
+                'pluginOptions' => [
+                    'allowClear' => TRUE,
+                    'minimumInputLength' => 4,
+                ]
+            ],
+            'pluginOptions' => [
+                'depends' => ['country-selection'],
+                'placeholder' => 'Select Location',
+                'url' => Url::to(['/contact/location/load'])
+            ]
+        ]);
+
+        $this->_printField($countryDropdown, $out);
+        $this->_printField($locationDropdown, $out);
+        return $out;
+    }
+
+    private function _printField($field, &$out)
+    {
+        $out .= $this->enableBootstrap ? '<div class="col-md-6">' : '';
+        $out .= $field;
+        $out .= $this->enableBootstrap ? '</div>' : '';
+    }
+
+}
