2de6c5e6cd9a8847bc7c88b5d33b32551d955826
jeffrey.geyssens <jeffrey@humanized.be>
2016-02-03 19:32:44 +0100

    [ADD] Import Controller and some free data files

---
+++ commands/ImportController.php
@@ -0,0 +1,251 @@
+<?php
+
+namespace humanized\location\commands;
+
+use humanized\clihelpers\controllers\ImportController as Controller;
+use humanized\location\models\location\City;
+use humanized\location\models\location\Country;
+use humanized\location\models\translation\CityTranslation;
+use humanized\location\models\translation\CountryTranslation;
+use humanized\locationmodels\location\Location;
+
+
+/**
+ * A CLI allowing Yii2 location managementS.
+ * 
+ * Supported commands:
+ * 
+ * > contact/import --<option>
+ *  --path
+ *  --delimiter
+ *  --enclosure
+ *  --terminator
+ *  --table
+ *  --autobuild
+ * 
+ * > contact/import/countries
+ * 
+ * 
+ * 
+ * @name Location Import CLI
+ * @version 0.1
+ * @author Jeffrey Geyssens <jeffrey@humanized.be>
+ * @package yii2-contact
+ * 
+ * 
+ * 
+ */
+class ImportController extends Controller {
+
+    private $_languages = [
+        'deu' => 'de',
+        'fra' => 'fr',
+        'hrv' => 'hr',
+        'ita' => 'it',
+        'jpn' => 'ja',
+        'nld' => 'nl',
+        'por' => 'pt',
+        'rus' => 'ru',
+        'spa' => 'es',
+        'fin' => 'fi',
+        'cym' => 'cy',
+    ];
+
+    public function actionNuts()
+    {
+        $this->path = \Yii::getAlias('@data') . '/nuts/nuts.csv';
+        $this->table = 'nuts';
+        // $this->actionIndex();
+        $p = \Yii::getAlias('@data') . '/nuts/pcc-free/';
+        /*
+          $result = fopen($p . 'correspondence', "w") or die("Unable to open file!");
+
+          foreach (scandir($p) as $file) {
+          if (substr($file, 0, 2) == 'pc') {
+          $country = strtoupper(substr($file, 3, 2));
+          echo 'Processing ' . $country . "\n";
+          $this->path = $p . $file;
+          $fx = fopen($p . $file, "r");
+          while (!feof($fx)) {
+          $record = fgetcsv($fx, 0,';');
+          if (isset($record[0])) {
+          fwrite($result, "\"$record[0]\";\"$record[1]\";\"$country\"\n");
+          } else {
+          break;
+          }
+          }
+          }
+          }
+          fclose($result);
+         * 
+         */
+        $this->delimiter = ';';
+        $this->path = $p . 'correspondence';
+        $this->table = 'nuts_correspondence';
+        $this->actionIndex();
+    }
+
+    public function actionImportBe()
+    {
+        $fileName = \Yii::getAlias('@data') . '/location/be.csv';
+        $file = fopen($fileName, "r");
+        while (!feof($file)) {
+            $record = fgetcsv($file, 0, ';');
+            if (isset($record[0])) {
+                $lang = in_array((int) substr($record[1], 0, 1), [1, 2, 3, 8, 9]) ? 'NL' : 'FR';
+                $city = new City(['language_id' => $lang]);
+                $city->save();
+                (new CityTranslation(['name' => $record[2], 'city_id' => $city->id, 'language_id' => 'fr']))->save();
+                (new CityTranslation(['name' => $record[3], 'city_id' => $city->id, 'language_id' => 'nl']))->save();
+                (new Location(['city_id' => $city->id, 'country_id' => 'BE', 'postcode' => $record[1]]))->save();
+            } else {
+                break;
+            }
+        }
+    }
+
+    public function actionImportDefault($cntry)
+    {
+        $fileName = \Yii::getAlias('@data') . "/location/$cntry.csv";
+        $file = fopen($fileName, "r");
+        fgetcsv($file, 0);
+        while (!feof($file)) {
+            $record = fgetcsv($file, 0, ';');
+            if (isset($record[0])) {
+                $city = new City(['language_id' => $record[3]]);
+                $city->save();
+                (new CityTranslation(['name' => $record[2], 'city_id' => $city->id, 'language_id' => $record[3]]))->save();
+                (new Location(['city_id' => $city->id, 'country_id' => $record[0], 'postcode' => $record[1]]))->save();
+            } else {
+                break;
+            }
+        }
+    }
+
+    /**
+     * 
+     * @param string $fn Absolute Path to Import Filename, if not set, the system will use a default import file loaded in the data folder in the root of the extension
+     * @param type $delimiter
+     * @return type
+     */
+    public function actionCountries($fn = NULL)
+    {
+        //Set filename to default location
+        if (!isset($fn)) {
+            $fn = \Yii::getAlias('@vendor') . '/humanized/yii2-contact/data/countries/countries.csv';
+        }
+        $file = fopen($fn, "r");
+        while (!feof($file)) {
+            $record = fgetcsv($file, 0);
+            if (!isset($record[0])) {
+                break;
+            }
+            $country = (new Country(['iso_2' => $record[0], 'iso_3' => $record[1], 'iso_numerical' => $record[2], 'has_postcodes' => $record[3]]));
+            $country->save();
+        }
+    }
+
+    /**
+     * 
+     */
+    public function actionCountryTranslations()
+    {
+        $fn = \Yii::getAlias('@vendor') . '/humanized/yii2-contact/data/countries/countries.json';
+        $json = file_get_contents($fn);
+        $object = json_decode($json);
+        foreach ($object as $record) {
+
+            (new Country(['iso_2' => $record->cca2, 'iso_3' => $record->cca3, 'has_postcodes' => 1, 'iso_numerical' => $record->ccn3]))->save();
+            //English Translation
+            (new CountryTranslation(['language_id' => 'en', 'country_id' => $record->cca2, 'official_name' => $record->name->official, 'common_name' => $record->name->common, 'demonym' => $record->demonym]))->save();
+            $this->_translateCountryRecord($record);
+        }
+        return 0;
+    }
+
+    private function _translateCountryRecord($record)
+    {
+        $code = $record->cca2;
+        foreach ($record->translations as $key => $translation) {
+
+            (new CountryTranslation(['country_id' => $code, 'language_id' => $this->_languages[$key], 'official_name' => $translation->official, 'common_name' => $translation->common,]))->save();
+        }
+    }
+
+    /**
+     * 
+     * Import Filename CSV must be provided in the following format:
+     * 
+     * [0]=> City Name
+     * 
+     * [1]=> Postal Code (Empty values will be threated as NULL)
+     * 
+     * [2]=> Country (iso-2 country code)
+     * 
+     * @param string $fn Absolute Path to Import Filename, if not set, the system will use a default import file loaded in the data folder in the root of the extension
+     * @param type $delimiter
+     * @return type
+     */
+    public function actionLocations($fn, $delimiter = ',')
+    {
+        $attributes = [
+            0 => 'city',
+            1 => 'postcode',
+            2 => 'country_id'];
+        $config = [
+            'path' => $fn,
+            'delimeter' => $delimiter,
+            'saveModel' => Location::className(),
+            'attributeMap' => $attributes
+        ];
+        return $this->importCSV($config);
+    }
+
+    public function actionCopyFromSource()
+    {
+        $chunkSize = 500;
+        $chunkPointer = 1;
+        $recordCount = \Yii::$app->db->createCommand('SELECT COUNT(*) FROM source')->queryScalar();
+        $unit = ($recordCount / 100);
+
+        $this->_msg = 'Processing ' . $recordCount . ' values';
+        $base = 'SELECT * FROM source';
+
+        $continue = true;
+
+        while ($continue) {
+            $qry = "$base LIMIT $chunkSize OFFSET $chunkPointer";
+            echo $qry;
+
+            $records = \Yii::$app->db->createCommand($qry)->queryAll();
+            foreach ($records as $record) {
+
+                if ($record['Country'] != 'BE' && $record['Country'] != 'NL') {
+                    $location = new Location([
+                        'country_id' => strtoupper($record['Country']),
+                        'postcode' => $record['Postcode'],
+                        'cityName' => $record['City'],
+                        'cityLanguage' => $record['Language']
+                    ]);
+
+                    echo $location->country_id . '::' . $location->postcode . '::' . $location->cityName . '::' . $location->cityLanguage . "\n";
+
+                    $location->save();
+                }
+            }
+            $this->_msg = ($chunkPointer / $chunkSize) . ' Percent Completed';
+            $this->printStatus();
+            $chunkPointer += $chunkSize;
+            if ($chunkPointer > $recordCount) {
+                $continue = false;
+            }
+        }
+        return 0;
+    }
+
+    public function actionMatchNuts()
+    {
+        Location::find()->all();
+    }
+
+}
